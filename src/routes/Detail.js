fb2947dcd664e484bcc6859ca9c650dc468db148
MyungjunLee <leesejk@naver.com>
2021-07-16 01:47:06 +0900

    third

---
+++ src/routes/Detail.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Detail extends React.Component{
+    componentDidMount(){
+        const { location, history } = this.props;
+        if(location.state === undefined){
+            history.push("/");
+        }
+    }
+    render(){
+        console.log(this.props);
+        const { location } = this.props;
+        if(location.state){
+            return <sapn>{location.state.title}</sapn>
+        } else {
+            return null;
+        }
+    }
+}
+export default Detail;
